d213b7b84c42676627f2f070ee1ffd47df08309e
ealinli <ealinli@tencent.com>
2021-07-09 14:09:28 +0800

    [UPD] add greater on cpu

---
+++ tools/onnx2tnn/src/core/layer/onnx_converter_greater.cc
@@ -0,0 +1,40 @@
+// Tencent is pleased to support the open source community by making TNN available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#include "onnx_op_converter.h"
+#include "onnx_utility.h"
+
+DECLARE_OP_CONVERTER_WITH_FUNC(Greater, virtual std::vector<std::string> GetValidInputNames(NodeProto &node,
+                                                                                            OnnxNetInfo &net_info););
+string OnnxOpConverterGreater::TNNOpType(NodeProto &node, OnnxNetInfo &net_info) {
+    return "Greater";
+}
+
+std::vector<std::string> OnnxOpConverterGreater::GetValidInputNames(NodeProto &node, OnnxNetInfo &net_info) {
+    return GetAllInputNames(node, net_info);
+}
+
+string OnnxOpConverterGreater::TNNLayerParam(NodeProto &node, OnnxNetInfo &net_info) {
+    return "";
+}
+
+bool OnnxOpConverterGreater::HasLayerResource(NodeProto &node, OnnxNetInfo &net_info) {
+    return false;
+}
+
+int OnnxOpConverterGreater::WriteTNNModel(Serializer *net_writer, NodeProto &node, OnnxNetInfo &net_info) {
+    return 0;
+}
+
+REGISTER_OP_CONVERTER(Greater, Greater);
